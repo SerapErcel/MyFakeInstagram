ad9ac17c89d57629b885c807b82e1ccd7f451fdb
sercel23 <ercelserap@gmail.com>
2022-03-03 23:46:45 +0300

    Create login screen

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun signInClicked(view: View) {}
+
+    fun signUpClicked(view: View) {}
 }
