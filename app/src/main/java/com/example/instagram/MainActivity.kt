7e19e77ce97505ac3b1b14cfa46b6df674f7f8ce
sercel23 <ercelserap@gmail.com>
2022-03-06 23:36:35 +0300

    Create user registration

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.instagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.instagram.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        auth = Firebase.auth
     }
 
     fun signInClicked(view: View) {}
 
-    fun signUpClicked(view: View) {}
+    fun signUpClicked(view: View) {
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Enter email and password!", Toast.LENGTH_LONG).show()
+
+        } else {
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                //success
+                val intent = Intent(this, FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
 }
