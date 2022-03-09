508ea77bbec02a1ff213b5efc587ec3c05d302cb
sercel23 <ercelserap@gmail.com>
2022-03-09 21:07:01 +0300

    Add firestore image upload

--- app/src/main/java/com/example/instagram/UploadActivity.kt
+++ app/src/main/java/com/example/instagram/UploadActivity.kt
@@ -15,6 +15,13 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.instagram.databinding.ActivityUploadBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 class UploadActivity : AppCompatActivity() {
 
@@ -22,6 +29,11 @@ class UploadActivity : AppCompatActivity() {
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
     var selectedPicture: Uri? = null
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +43,25 @@ class UploadActivity : AppCompatActivity() {
 
         registerLauncher()
 
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
+
+
     }
 
-    fun upload(view: View) {}
+    fun upload(view: View) {
+        val reference=storage.reference
+        val imageReference=reference.child("images/image.jpg")
+
+        if( selectedPicture!=null){
+            imageReference.putFile(selectedPicture!!).addOnSuccessListener {
+                //download url -> firestore
+            }.addOnFailureListener{
+                Toast.makeText(this, it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
     fun selectImage(view: View) {
         if (ContextCompat.checkSelfPermission(
